af797d76910f3ef8c291fd132f94c68e1037a364
WaelS87 <wfakhrji@gmail.com>
2022-11-11 15:59:01 -0300

    style y classes front js

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,26 @@ const main = $('mainContainer')
 const subtitulo = qs('.subtitulo')
 const parrafos = qsa('p')
 const listado = qs('h2 a')
+
+let nombre = prompt('ingres tu nombre por fa!!!')
+
+subtitulo.textContent += nombre ? nombre :"invitado"
+subtitulo.style.textTransform ="uppercase"
+
+listado.style.color="#E51B3E"
+
+let respuesta = confirm('¿quieres cambiar el fondo?')
+if(respuesta){
+   qs('body').classList.toggle('fondo')}
+parrafos.forEach((parrafo,index)=>{
+if(index % 2 === 0 ){
+    parrafo.classList.add('destacadoImpar')
+}else{
+    parrafo.classList.add('destacadoPar')
+}
+
+})
+
+
+
+main.style.display ="block"
